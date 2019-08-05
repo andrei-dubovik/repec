3077ce94573fb661130fd2cc89d149b74fc2d82f
Andrei Dubovik <andrei@dubovik.eu>
2019-08-05 12:25:34 +0200

    Forgot to change main.py to include papers updating

--- repec/main.py
+++ repec/main.py
@@ -1,10 +1,12 @@
 # Load local packages
 import repec
 import remotes
+import papers
 
 def update():
     '''Run full database update'''
     repec.update()
     remotes.update()
+    papers.update()
 
 # update()
